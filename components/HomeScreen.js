b7a8ed11ad319d9c3c6409b8da913ba72f34448d
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-08-24 19:18:14 +1000

    Problem occured

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,13 +1,48 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Button, FlatList } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 export const HomeScreen = (props) => {
     const navigation = useNavigation() //Referencing
+    
+    const renderList = ({item}) => (
+        <ListItem id={item.id} amount={item.amount} category={item.category} />
+    )
+
+    //To see...
+    const showDetail = ( item ) =>{
+
+    }
+
     return (
         <View>
             <Text>{props.text}</Text>
-            <Button title ="Go to Detail" onPress={() => {navigation.navigate("Detail")}} />
+            {/* <Button title ="Go to Detail" onPress={() => {navigation.navigate("Detail")}} /> */}
+            <FlatList 
+            data = { props.data} 
+            renderItem = { renderList }
+            keyExtractor = { item => item.id }
+            />
         </View>
     )
 }
+
+const ListItem = (props) => {
+    return (
+        <View style={homeStyle.item}>
+            <Text>{props.category}</Text>
+            <Text>$ {props.amount}</Text>
+        </View>
+    )
+}
+
+const homeStyle = StyleSheet.create({
+    item:{
+        paddingHorizontal: 15,
+        paddingVertical: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
+        flexDirection: 'row',
+
+    },
+})
