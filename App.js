00e2b8661acc5a293167a5c3f2f3737e865623e0
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-08-24 19:52:19 +1000

    Still some issue

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import{ NavigationContainer, StackActions} from '@react-navigation/native';
-import{ createStackNavigator } from '@react-navigation/stack';
+import{ NavigationContainer, StackActions} from '@react-navigation/native'
+import{ createStackNavigator } from '@react-navigation/stack'
 
-import { HomeScreen } from './components/HomeScreen';
-import { DetailScreen } from './components/DetailScreen';
+import { HomeScreen } from './components/HomeScreen'
+import { DetailScreen } from './components/DetailScreen'
+
+const Data = [
+  {  "amount": 50,  "category": "food",  "id": "1598241633",  "note": "buying lunch"},
+  {  "amount": 20,  "category": "transport",  "id": "1598241768",  "note": "catching train"},
+  {  "amount": 80,  "category": "groceries",  "id": "1598241782",  "note": "shopping at Coles"},
+  {  "amount": 13,  "category": "food",  "id": "1598241795",  "note": "snack time"},
+  {  "amount": 35,  "category": "entertainment",  "id": "1598241806",  "note": "buying Untitled Goose"},
+  {  "amount": 350,  "category": "rent",  "id": "1598241817",  "note": "weeks rent"},
+  {  "amount": 60,  "category": "transport",  "id": "1598241827",  "note": "topping up Opal card"},
+  {  "amount": 30,  "category": "food",  "id": "1598241841",  "note": "buying dinner"}
+  ]
 
 export default function App() {
+  const listData = Data
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home">
-          { (props) => <HomeScreen {...props} text="Hello Home Screen"/>}
+          { (props) => <HomeScreen {...props} text="Hello Home Screen" data={listData}/>}
         </Stack.Screen>
-        {/*The above is passing props from HomeScreen.js. This is original:<Stack.Screen name="Home" component={HomeScreen} /> */}
         <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- components/DetailScreen.js
+++ components/DetailScreen.js
@@ -5,6 +5,10 @@ export const DetailScreen = ( props ) => {
     return (
         <View>
             <Text>Detail page</Text>
+            <Text>id: {props.route.params.id}</Text>
+            <Text>amount: {props.route.params.amount}</Text>
+            <Text>category: {props.route.params.category}</Text>
+            <Text>note: {props.route.params.note}</Text>
         </View>
     )
 }
