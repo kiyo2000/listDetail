3033757a566ae871598750352210b2fbccc5fd0f
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-08-31 17:33:54 +1000

    Added  config file with firebase api

---
+++ config/firebase.js
@@ -0,0 +1,10 @@
+export const  firebaseConfig = {
+    apiKey: "AIzaSyBWjR7CqDF1oSN-Ma1jGxNs5dpG-6p3wmI",
+    authDomain: "firetest-b8112.firebaseapp.com",
+    databaseURL: "https://firetest-b8112.firebaseio.com",
+    projectId: "firetest-b8112",
+    storageBucket: "firetest-b8112.appspot.com",
+    messagingSenderId: "260427426146",
+    appId: "1:260427426146:web:1ba5967debe26902d3a43b",
+    measurementId: "G-W9ZGVKQ3SW"
+  };
